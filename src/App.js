e3ac7787b3038580c599b0e250b8335bee34d4bb
Oscar Maia <oscarfgmaia@gmail.com>
2022-11-16 23:21:30 -0300

    layout Expense Page finished

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import GlobalStyle from "./assets/css/GlobalStyle";
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
 import RegisterPage from "./pages/RegisterPage";
-
+import EntryPage from "./pages/EntryPage"
+import ExpensePage from "./pages/ExpensePage";
 export default function App() {
   return (
     <BrowserRouter>
@@ -12,6 +13,8 @@ export default function App() {
           <Route path="/" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/main" element={<MainPage />} />
+          <Route path="/entry" element={<EntryPage />} />
+          <Route path="/expense" element={<ExpensePage />} />
         </Routes>
     </BrowserRouter>
 
---
+++ src/pages/EntryPage.js
@@ -0,0 +1,74 @@
+import styled from "styled-components";
+
+export default function EntryPage() {
+  return (
+    <CenteredContainer>
+        <CenteredDiv>
+          <h1>Nova entrada</h1>
+          <StyledInput placeholder="Valor"></StyledInput>
+          <StyledInput placeholder="Descrição"></StyledInput>
+          <StyledButton>Salvar Entrada</StyledButton>
+        </CenteredDiv>
+    </CenteredContainer>
+  );
+}
+
+const CenteredContainer = styled.div`
+    margin-top: 25px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledInput = styled.input`
+  font-family: "Raleway", sans-serif;
+  font-size: 20px;
+  color: #000;
+
+  box-sizing: border-box;
+  border: none;
+  padding: 0px;
+  width: 326px;
+  height: 58px;
+  background: #ffffff;
+
+  border-radius: 5px;
+  margin-bottom: 13px;
+  padding-left: 15px;
+  padding-right: 15px;
+  ::placeholder {
+    font-family: "Raleway", sans-serif;
+  }
+`;
+
+const StyledButton = styled.button`
+  font-family: "Raleway", sans-serif;
+  box-sizing: border-box;
+  border: none;
+  width: 326px;
+  height: 58px;
+  background: #a328d6;
+  border-radius: 5px;
+  margin-bottom: 36px;
+
+  font-size: 20px;
+  font-weight: 700;
+  color: #fff;
+`;
+
+const CenteredDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+  h1 {
+    font-family: 'Raleway';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 26px;
+    line-height: 31px;
+    color: #FFFFFF;
+    margin-bottom: 40px;
+  }
+`;
