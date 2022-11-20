c702db7ca2398e10227fbfd7b743821cb44a522d
Oscar Maia <oscarfgmaia@gmail.com>
2022-11-20 16:45:31 -0300

    alert error in register

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -20,7 +20,7 @@ export default function RegisterPage() {
           navigate('/');
         })
         .catch(err => {
-          console.log(err)
+          alert(err.response.data)
         })
     }
   }
